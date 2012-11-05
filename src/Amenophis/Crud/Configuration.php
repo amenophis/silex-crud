7140046a8a7bbe3130f739e49b4dc21817c4d7f9
Jérémy LEHERPEUR <amenophis@leherpeur.net>
2012-11-06 00:11:25 +0100

    Add layout option

--- src/Amenophis/Crud/Configuration.php
+++ src/Amenophis/Crud/Configuration.php
@@ -14,6 +14,7 @@ class Configuration implements ConfigurationInterface
 
         $rootNode
             ->children()
+                ->scalarNode('layout')->end()
                 ->arrayNode('list')
                     ->children()
                         ->variableNode('fields')->end()
